43ac5d225379e8b9c201087a1fb7ca4afc1449f4
Joe Minichino <joe.minichino@gmail.com>
2014-10-02 22:52:26 +0100

    first draft of interface

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 var net = require('net'),
-	client = new net.Socket();
+	client = new net.Socket(),
+	readline = require('readline'),
+	op = require('./operations.js');
 
 client.connect(47134, '127.0.0.1');
-client.write(JSON.stringify({
-	op: 3000,
-	collection: 'users',
-	obj: {
-		name: 'joe'
-	}
-}), 'utf8');
 client.on('data', function (data) {
-	var obj = JSON.parse(data);
-	console.log(obj);
-	client.end();
+	console.log(data.toString());
+	input.prompt();
+});
+
+var input = readline.createInterface(process.stdin, process.stdout);
+input.setPrompt('loki> ');
+input.prompt();
+input.on('line', function (line) {
+	var pieces = line.split(' ', 2),
+		obj;
+	try {
+		obj = JSON.parse(line.replace(pieces.join(' '), ''));
+		var message = {
+			collection: pieces[1],
+			op: op['OP_' + pieces[0].toUpperCase()],
+			obj: obj
+		};
+		client.write(JSON.stringify(message), 'utf8');
+
+	} catch (err) {
+		console.error(err);
+	}
+	input.prompt();
+}).on('close', function () {
+	process.exit(0);
 });
